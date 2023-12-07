49ebaac6c2069560e6cc617ddb98bc8fd2fa09bb
Santiago Cuebas <sanlugus497@gmail.com>
2023-12-07 14:16:41 -0300

    Minor change

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -1,10 +1,5 @@
 import type { LayoutServerLoad } from './$types';
 
-export const config = {
-	runtime: 'edge',
-	regions: 'all'
-};
-
 export const load = (async ({ locals }) => {
 	return {
 		user: locals.user
