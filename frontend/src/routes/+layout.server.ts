7bff11495eaa5ad38d6a60febd60970cca863e76
Santiago Cuebas <sanlugus497@gmail.com>
2023-12-25 08:28:15 -0300

    Miinor change

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -4,14 +4,13 @@ import { DIR } from '$lib/config';
 
 export const load: LayoutServerLoad = (async ({ cookies }) => {
 	const token = cookies.get('authenticate');
-	const config = {
+	
+	return axios({
 		method: 'GET',
 		url: `${DIR}/api/auth`,
-		headers: { 'Cookie': `authenticate=${token}` }
-	};
-	
-	return axios(config)
-		.then(res => res.data)
+		headers: { 'Cookie': `authenticate=${token}` },
+		proxy: false
+	}).then(res => res.data)
 		.catch(err => {
 			console.error(err.message);
 			return { user: undefined };
