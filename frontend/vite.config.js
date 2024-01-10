ce63ae13eb17ed93f522fd02e02db069552e513e
Santiago Cuebas <sanlugus497@gmail.com>
2024-01-10 16:33:36 -0300

    Minor change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,22 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
+	resolve: {
+		alias: {
+			http: 'rollup-plugin-node-polyfills/polyfills/http',
+			https: 'rollup-plugin-node-polyfills/polyfills/http',
+			util: 'rollup-plugin-node-polyfills/polyfills/util',
+			zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
+			stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+			events: 'rollup-plugin-node-polyfills/polyfills/events',
+			path: 'rollup-plugin-node-polyfills/polyfills/path',
+			url: 'rollup-plugin-node-polyfills/polyfills/url',
+			fs: 'rollup-plugin-node-polyfills/polyfills/fs',
+			assert: 'rollup-plugin-node-polyfills/polyfills/assert',
+			tty: 'rollup-plugin-node-polyfills/polyfills/tty',
+			os: 'rollup-plugin-node-polyfills/polyfills/os'
+		}
+	},
 	plugins: [sveltekit()],
 	server: {
 		port: 3000
