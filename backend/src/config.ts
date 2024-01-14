28a1a86ef6b1e953acb3a848bb8a35eeb4c31c9d
Santiago Cuebas <sanlugus497@gmail.com>
2024-01-14 06:07:59 -0300

    Minor change

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT ?? 4200;
 
 export const DB_HOST = process.env.DB_HOST;
 
-export const DB_PORT = process.env.DB_PORT || 3306 as number;
+export const DB_PORT = process.env.DB_PORT ?? 3306;
 
 export const DB_USER = process.env.DB_USER;
 
